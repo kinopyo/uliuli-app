cc8d56899edfce628476b41ec38ed5767d4bee3d
kinopyo <fantasyday@gmail.com>
2016-03-26 13:54:13 +0900

    Initial Commit

---
+++ uliuli-app/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  uliuli-app
+//
+//  Created by qihuan-piao on 2016/03/26.
+//  Copyright © 2016年 qihuan-piao. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
